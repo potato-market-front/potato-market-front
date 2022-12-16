9261dcc0e1a450dbc5c09d5330ef6c2bc9ed757d
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-17 03:14:25 +0900

    Router Path 설정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "../pages/Main";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Create from '../pages/Create';
+import Main from '../pages/Main';
+import Update from '../pages/Update';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/create' element={<Create />} />
+        <Route path='/update/:productId' element={<Update />} />
       </Routes>
     </BrowserRouter>
   );
