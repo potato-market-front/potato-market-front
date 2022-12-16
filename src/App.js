3cb0713e5f6a262ae044b52503a5b0be45638ba1
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-17 03:24:10 +0900

    Router 설정 수정

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 
-export default function Main() {
-  return (
-    <div className='container'>
-      {/* header */}
-      <header className='header'>
-        <h1>메인페이지</h1>
-      </header>
-      {/* images */}
-      <StImgWrapper>
-        <div className='image container'>
-          <StImg src='https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166893084493933827.jpg?w=2048&h=1280&c=c'></StImg>
-        </div>
-        <div className='image container'>
-          <StImg src='https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166893084493933827.jpg?w=2048&h=1280&c=c'></StImg>
-        </div>
-        <div className='image container'>
-          <StImg src='https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166893084493933827.jpg?w=2048&h=1280&c=c'></StImg>
-        </div>
-      </StImgWrapper>
-    </div>
-  );
+import Router from './shared/Router';
+
+function App() {
+  return <Router />;
 }
-
-const StImgWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 0 20px;
-`;
-const StImg = styled.img`
-  width: 100%;
-`;
+export default App;
