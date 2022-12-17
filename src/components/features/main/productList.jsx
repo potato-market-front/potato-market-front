aeb1c85113eb136df99c192e4eebcb108e5fcc21
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-17 15:43:23 +0900

    json 서버에서 products GET

--- src/components/features/main/productList.jsx
+++ src/components/features/main/productList.jsx
@@ -1,55 +1,96 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import React, { useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Card from './Card';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
+// const CARD_LIST = [
+//   {
+//     id: '1',
+//     title: 'Ugg',
+//     image:
+//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+//     price: 120000,
+//     createdAt: '2022-11-30T17:45:43.726338',
+//     modifiedAt: '2022-11-30T17:45:43.726338',
+//   },
+//   {
+//     id: '2',
+//     title: 'Ugg',
+//     image:
+//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+//     price: 120000,
+//     createdAt: '2022-11-30T17:45:43.726338',
+//     modifiedAt: '2022-11-30T17:45:43.726338',
+//   },
+//   {
+//     id: '3',
+//     title: 'Ugg',
+//     image:
+//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+//     price: 120000,
+//     createdAt: '2022-11-30T17:45:43.726338',
+//     modifiedAt: '2022-11-30T17:45:43.726338',
+//   },
+//   {
+//     id: '4',
+//     title: 'Ugg',
+//     image:
+//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+//     price: 120000,
+//     createdAt: '2022-11-30T17:45:43.726338',
+//     modifiedAt: '2022-11-30T17:45:43.726338',
+//   },
+//   {
+//     id: '5',
+//     title: 'Ugg',
+//     image:
+//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+//     price: 120000,
+//     createdAt: '2022-11-30T17:45:43.726338',
+//     modifiedAt: '2022-11-30T17:45:43.726338',
+//   },
+//   {
+//     id: '6',
+//     title: 'Ugg',
+//     image:
+//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+//     price: 120000,
+//     createdAt: '2022-11-30T17:45:43.726338',
+//     modifiedAt: '2022-11-30T17:45:43.726338',
+//   },
+//   {
+//     id: '7',
+//     title: 'airPod',
+//     image:
+//       'https://www.apple.com/newsroom/images/product/airpods/standard/Apple-AirPods-Pro-2nd-gen-hero-220907_big.jpg.large.jpg',
+//     price: 220000,
+//     createdAt: '2022-11-30T17:45:43.726338',
+//     modifiedAt: '2022-11-30T17:45:43.726338',
+//   },
+// ];
 
 export default function ProductList() {
+  const [cardList, setCardList] = useState([]);
+
+  const getProductsList = useCallback(async () => {
+    const { data } = await axios.get('http://localhost:8080/products');
+    console.log(data);
+    setCardList(data);
+  }, []);
+
+  useEffect(() => {
+    getProductsList();
+  }, [getProductsList]);
+
   return (
-    <div className="container">
-      {/* images */}
+    <div className='container'>
       <StImgWrapper>
-        <div className="image container">
-          <StImg src="https://dnvefa72aowie.cloudfront.net/origin/article/202212/0ECB5AA09544861676572ED085C0DA57BDCED6102D96C73DDD2D9FAADC63CC9A.jpg?q=82&s=300x300&t=crop"></StImg>
-          <StContentWrapper>
-            <div>내용:</div>
-            <div>가격:</div>
-          </StContentWrapper>
-        </div>
-        <div className="image container">
-          <StImg src="https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop"></StImg>
-          <StContentWrapper>
-            <div>내용:</div>
-            <div>가격:</div>
-          </StContentWrapper>
-        </div>
-        <div className="image container">
-          <StImg src="https://dnvefa72aowie.cloudfront.net/origin/article/202212/01d957c88a7ec250747e107e221599556b5a02f4c9a891382bbf6c073f3f0881.webp?q=95&s=1440x1440&t=inside"></StImg>
-          <StContentWrapper>
-            <div>내용:</div>
-            <div>가격:</div>
-          </StContentWrapper>
-        </div>
-        <div className="image container">
-          <StImg src="https://dnvefa72aowie.cloudfront.net/origin/article/202212/01d957c88a7ec250747e107e221599556b5a02f4c9a891382bbf6c073f3f0881.webp?q=95&s=1440x1440&t=inside"></StImg>
-          <StContentWrapper>
-            <div>내용:</div>
-            <div>가격:</div>
-          </StContentWrapper>
-        </div>
-        <div className="image container">
-          <StImg src="https://dnvefa72aowie.cloudfront.net/origin/article/202212/01d957c88a7ec250747e107e221599556b5a02f4c9a891382bbf6c073f3f0881.webp?q=95&s=1440x1440&t=inside"></StImg>
-          <StContentWrapper>
-            <div>내용:</div>
-            <div>가격:</div>
-          </StContentWrapper>
-        </div>
-        <div className="image container">
-          <StImg src="https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop"></StImg>
-          <StContentWrapper>
-            <div>내용:</div>
-            <div>가격:</div>
-          </StContentWrapper>
-        </div>
+        {cardList.map((v, i) => (
+          <Card key={v.id} imgSrc={v.image} title={v.title} price={v.price} />
+        ))}
       </StImgWrapper>
     </div>
   );
@@ -58,15 +99,8 @@ export default function ProductList() {
 const StImgWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  gap: 0 20px;
+  gap: 20px 20px;
   margin-top: 10px;
   margin: 0 auto;
   max-width: 600px;
-`;
-const StContentWrapper = styled.div`
-  margin-top: 5px;
-  font-size: 10px;
-`;
-const StImg = styled.img`
-  width: 100%;
 `;
