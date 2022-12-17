cbf9140fccd17cdc646ed1f54bf042d638002d96
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-17 12:09:55 +0900

    Header 버튼 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,15 +1,18 @@
-import styled from "styled-components";
-import { COLORS } from "../../styles/colors";
-// import Logo from "../../../assets/png/potato.png";
+import styled from 'styled-components';
+import { COLORS } from '../../styles/colors';
+import Logo from '../../assets/png/potato.png';
+import Button from './Button';
 
 export default function Header() {
   return (
     <StHeader>
       <StIconWrap>
-        {/* <img alt="logo" src={Logo} width="40px" /> */}
+        <img alt='logo' src={Logo} width='40px' />
         <StLogo>감자마-켓</StLogo>
       </StIconWrap>
-      <button>로그아웃</button>
+      <Button width={'120px'} height={'30px'}>
+        로그아웃
+      </Button>
     </StHeader>
   );
 }
