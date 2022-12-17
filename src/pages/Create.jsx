3e47a1ee26b7a8ea2d40f2c7320294a81ed48c99
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-17 16:19:56 +0900

    detail page router param 출력

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from '../components/common/Layout';
-
 import Header from '../components/common/Header';
 import CreateProduct from '../components/features/create/CreateProduct';
 
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import CommentList from "../components/features/detail/commentList";
-import ProductDetail from "../components/features/detail/productDetail";
-import CreateComment from "../components/features/detail/createComment";
+import React from 'react';
+import styled from 'styled-components';
+import CommentList from '../components/features/detail/commentList';
+import ProductDetail from '../components/features/detail/productDetail';
+import CreateComment from '../components/features/detail/createComment';
+import { useParams } from 'react-router-dom';
 
 const Div = styled.div`
   padding: 10px;
@@ -11,19 +12,20 @@ const Div = styled.div`
 `;
 
 function Detail() {
+  const { productId } = useParams();
   return (
-    <Div className="container">
-      <div className="header">
+    <Div className='container'>
+      <div className='header'>
         header
         <h3>상세페이지</h3>
-        <Div className="상품정보및수정">
+        <Div className='상품정보및수정'>
           Main에서 선택한 게시글
           <button>(link to)게시글 수정</button>
-          <ProductDetail className="게시글정보" />
+          <ProductDetail className='게시글정보' />
         </Div>
-        <div className="댓글">
-          <CreateComment className="댓글쓰는공간" />
-          <CommentList className="댓글공간" />
+        <div className='댓글'>
+          <CreateComment className='댓글쓰는공간' />
+          <CommentList className='댓글공간' />
         </div>
       </div>
     </Div>
