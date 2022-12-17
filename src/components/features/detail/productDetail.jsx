819f08ab6d8d9b0f1abce8f1166f37b46ae171ae
SeungwooYang <dg6800000@gmail.com>
2022-12-16 21:21:19 -0800

    Create productDetail.jsx
    상세페이지 속 게시글 정보 조회 컴포넌트 분리

---
+++ src/components/features/detail/productDetail.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  padding: 10px;
+  border: 1px solid red;
+  margin: 5px;
+`;
+const InheritedDiv = styled(Div)`
+  border: 1px solid red;
+`;
+function ProductDetail() {
+  return (
+    <Div>
+      게시글 조회
+      <InheritedDiv>사진이 보여지는 곳</InheritedDiv>
+      <div>제목: 값</div>
+      <div>작성자: 값</div>
+      <div>가격: 값</div>
+      <div>글 내용: 값</div>
+    </Div>
+  );
+}
+export default ProductDetail;
