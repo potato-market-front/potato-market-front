bd381b7dc952be0090905cbaa1c09a893d398889
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-19 09:22:23 +0900

    ProductForm type 지정

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from '../components/common/Layout';
 import Header from '../components/common/Header';
-import CreateProduct from '../components/features/create/createProduct';
+import ProductForm from '../components/features/create/ProductForm';
 
 export default function Create() {
   return (
     <>
       <Header />
       <Layout>
-        <CreateProduct />
+        <ProductForm type='create' />
       </Layout>
     </>
   );
