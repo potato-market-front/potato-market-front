715c8f283482b40e2cebb71b50a6070ca5fa2b87
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-19 20:19:17 +0900

    products 데이터 수정(일단 변수로)

--- src/components/features/detail/ProductDetail.jsx
+++ src/components/features/detail/ProductDetail.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import { COLORS, GRAY_COLORS } from '../../../styles/colors';
 
-import DetailProductForm from '../create/DetailProductForm';
-
-function ProductDetail() {
+export default function ProductDetail() {
   return (
-    <div>
-      <DetailProductForm />
-    </div>
+    <StDetilProductContainer>
+      <StImgContainer>사진</StImgContainer>
+      <StSpan>제목</StSpan>
+      <StSpan>가격</StSpan>
+      <StTextarea>글내용</StTextarea>
+    </StDetilProductContainer>
   );
 }
-export default ProductDetail;
+
+const StDetilProductContainer = styled.section`
+  width: 320px;
+  margin: 0 auto;
+`;
+const StImgContainer = styled.div`
+  width: 100%;
+  height: 160px;
+  text-align: center;
+  border: 1px ${COLORS.POSITIVE} solid;
+  color: ${COLORS.POINT};
+  border-radius: 8px;
+  margin-bottom: 16px;
+`;
+const StSpan = styled.span`
+  display: block;
+  width: 100%;
+  padding: 4px 8px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+  border: gray solid 0.5px;
+`;
+const StTextarea = styled.textarea`
+  width: 100%;
+  padding: 4px 8px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+  border: ${GRAY_COLORS.GRAYD} solid 0.5px;
+`;
--- src/components/features/main/ProductList.jsx
+++ src/components/features/main/ProductList.jsx
@@ -1,94 +1,94 @@
-import React, { useCallback } from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import Card from "./Card";
-import { useState } from "react";
-import { useEffect } from "react";
-import axios from "axios";
+import React, { Profiler, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Card from './Card';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-// const CARD_LIST = [
-//   {
-//     id: '1',
-//     title: 'Ugg',
-//     image:
-//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
-//     price: 120000,
-//     createdAt: '2022-11-30T17:45:43.726338',
-//     modifiedAt: '2022-11-30T17:45:43.726338',
-//   },
-//   {
-//     id: '2',
-//     title: 'Ugg',
-//     image:
-//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
-//     price: 120000,
-//     createdAt: '2022-11-30T17:45:43.726338',
-//     modifiedAt: '2022-11-30T17:45:43.726338',
-//   },
-//   {
-//     id: '3',
-//     title: 'Ugg',
-//     image:
-//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
-//     price: 120000,
-//     createdAt: '2022-11-30T17:45:43.726338',
-//     modifiedAt: '2022-11-30T17:45:43.726338',
-//   },
-//   {
-//     id: '4',
-//     title: 'Ugg',
-//     image:
-//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
-//     price: 120000,
-//     createdAt: '2022-11-30T17:45:43.726338',
-//     modifiedAt: '2022-11-30T17:45:43.726338',
-//   },
-//   {
-//     id: '5',
-//     title: 'Ugg',
-//     image:
-//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
-//     price: 120000,
-//     createdAt: '2022-11-30T17:45:43.726338',
-//     modifiedAt: '2022-11-30T17:45:43.726338',
-//   },
-//   {
-//     id: '6',
-//     title: 'Ugg',
-//     image:
-//       'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
-//     price: 120000,
-//     createdAt: '2022-11-30T17:45:43.726338',
-//     modifiedAt: '2022-11-30T17:45:43.726338',
-//   },
-//   {
-//     id: '7',
-//     title: 'airPod',
-//     image:
-//       'https://www.apple.com/newsroom/images/product/airpods/standard/Apple-AirPods-Pro-2nd-gen-hero-220907_big.jpg.large.jpg',
-//     price: 220000,
-//     createdAt: '2022-11-30T17:45:43.726338',
-//     modifiedAt: '2022-11-30T17:45:43.726338',
-//   },
-// ];
+const CARD_LIST = [
+  {
+    id: '1',
+    title: 'Ugg',
+    image:
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+    price: 120000,
+    createdAt: '2022-11-30T17:45:43.726338',
+    modifiedAt: '2022-11-30T17:45:43.726338',
+  },
+  {
+    id: '2',
+    title: 'Ugg',
+    image:
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+    price: 120000,
+    createdAt: '2022-11-30T17:45:43.726338',
+    modifiedAt: '2022-11-30T17:45:43.726338',
+  },
+  {
+    id: '3',
+    title: 'Ugg',
+    image:
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+    price: 120000,
+    createdAt: '2022-11-30T17:45:43.726338',
+    modifiedAt: '2022-11-30T17:45:43.726338',
+  },
+  {
+    id: '4',
+    title: 'Ugg',
+    image:
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+    price: 120000,
+    createdAt: '2022-11-30T17:45:43.726338',
+    modifiedAt: '2022-11-30T17:45:43.726338',
+  },
+  {
+    id: '5',
+    title: 'Ugg',
+    image:
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+    price: 120000,
+    createdAt: '2022-11-30T17:45:43.726338',
+    modifiedAt: '2022-11-30T17:45:43.726338',
+  },
+  {
+    id: '6',
+    title: 'Ugg',
+    image:
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202212/F4C802A00FB1B732CD39B1DE901A8D0BD5929CD3D51B3756FE7243F5ABEE6791.jpg?q=82&s=300x300&t=crop',
+    price: 120000,
+    createdAt: '2022-11-30T17:45:43.726338',
+    modifiedAt: '2022-11-30T17:45:43.726338',
+  },
+  {
+    id: '7',
+    title: 'airPod',
+    image:
+      'https://www.apple.com/newsroom/images/product/airpods/standard/Apple-AirPods-Pro-2nd-gen-hero-220907_big.jpg.large.jpg',
+    price: 220000,
+    createdAt: '2022-11-30T17:45:43.726338',
+    modifiedAt: '2022-11-30T17:45:43.726338',
+  },
+];
 
 export default function ProductList() {
-  const [cardList, setCardList] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  // // const getProductsList = useCallback(async () => {
-  // //   const { data } = await axios.get("http://localhost:3001/products");
-  // //   console.log(data);
-  // //   setCardList(data);
-  // // }, []);
+  const getProductsList = useCallback(async () => {
+    const { data } = await axios.get('http://localhost:3001/products');
+    setProducts(data);
+  }, []);
 
-  // useEffect(() => {
-  //   getProductsList();
-  // }, [getProductsList]);
+  useEffect(() => {
+    // getProductsList();
+    setProducts(CARD_LIST);
+  }, [getProductsList]);
 
   return (
-    <div className="container">
+    <div className='container'>
       <StImgWrapper>
-        {cardList.map((v) => (
+        {products.map((v) => (
           <Card
             key={v.id}
             id={v.id}
@@ -104,7 +104,7 @@ export default function ProductList() {
 
 const StImgWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;                                      
+  grid-template-columns: 1fr 1fr 1fr;
   gap: 20px 20px;
   margin-top: 10px;
   margin: 0 auto;
