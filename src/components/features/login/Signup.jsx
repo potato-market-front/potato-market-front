fe299c0af18ed536f29c6dce03429766ba7cd711
HaileyKim <kimmy199535@gmail.com>
2022-12-20 14:17:43 +0900

    Update Signup.jsx
    signup 서버 연결 부분 추가

--- src/components/features/login/Signup.jsx
+++ src/components/features/login/Signup.jsx
@@ -1,50 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../../common/Input";
 import Button from "../../common/Button";
+import {
+  idDupCheck,
+  nickDupCheck,
+  postSignup,
+} from "../../../redux/modules/login";
+import TextButton from "../../common/TextButton";
 
 function SignUp() {
+  const [id, setId] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigation = useNavigate();
+
+  const onSignUp = () => {
+    postSignup({
+      id,
+      nickname,
+      password,
+    }).then((res) => {
+      localStorage.setItem("id", res.headers.authorization);
+      navigation("/login");
+    });
+  };
+
+  const idDup = (id) => {
+    idDupCheck(id).then((res) => {
+      console.log("ID 중복:", res);
+      if (res === false) {
+        alert("사용 가능한 ID입니다.");
+        setId(res);
+      } else {
+        alert("이미 사용중인 ID입니다.");
+        setId(res);
+        id("");
+      }
+    });
+  };
+
+  const nickDup = (nickname) => {
+    nickDupCheck(nickname).then((res) => {
+      console.log("Nick 중복:", res);
+      if (res === false) {
+        alert("사용 가능한 Nickname입니다.");
+        setNickname(res);
+      } else {
+        alert("이미 사용중인 Nickname입니다.");
+        setNickname(res);
+        nickname("");
+      }
+    });
+  };
+
   return (
     <StTopContainer>
       <h1>감자마켓 회원가입</h1>
       <StInputGroup>
-        <div>
+        <StInputnButton>
           <Input
+            onChange={(event) => {
+              setId(event.target.value);
+            }}
             type="text"
             name="id"
             label="ID를 입력하세요."
             width={"250px"}
           ></Input>
-        </div>
-        <div>
+          <TextButton onClick={idDup}>중복확인</TextButton>
+        </StInputnButton>
+        <StInputnButton>
           <Input
+            onChange={(event) => {
+              setNickname(event.target.value);
+            }}
             type="text"
             name="nick"
             label="NickName을 입력하세요."
             width={"250px"}
           ></Input>
-        </div>
+          <TextButton onClick={nickDup}>중복확인</TextButton>
+        </StInputnButton>
         <div>
           <Input
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
             type="password"
             name="password"
             label="비밀번호를 입력하세요."
             width={"250px"}
           ></Input>
         </div>
-        <div>
-          <Input
-            type="password"
-            name="confirm"
-            label="비밀번호를 확인해주세요."
-            width={"250px"}
-          ></Input>
-        </div>
       </StInputGroup>
       <StButtonGroup>
         <div>
-          <Button width={"250px"}>Sign Up</Button>
+          <Button width={"250px"} onClick={onSignUp}>
+            Sign Up
+          </Button>
         </div>
         <StLink>
           <Link to="/login">
@@ -65,7 +121,6 @@ const StTopContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 100px;
-
   gap: 50px;
 `;
 
@@ -73,7 +128,6 @@ const StInputGroup = styled.div`
   display: flex;
   flex-direction: column;
   margin: auto;
-
   gap: 30px;
 `;
 
@@ -82,7 +136,6 @@ const StButtonGroup = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
   margin: auto;
   gap: 20px;
 `;
@@ -93,4 +146,11 @@ const StLink = styled.div`
     text-decoration: none;
     color: #4d4f50;
   }
+`;
+
+const StInputnButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  margin-left: 65px;
 `;
